77b15fccf26da7867af6f4a628f1e3c4277d66d8
Fabian Flores <fabian.flores@incode.com>
2021-08-13 07:49:40 -0500

    add in app browser

--- App.tsx
+++ App.tsx
@@ -1,21 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, {useState} from 'react'
+import {
+  TouchableOpacity,
+  Text,
+  View,
+  StyleSheet,
+  Dimensions,
+} from 'react-native'
+import * as WebBrowser from 'expo-web-browser'
+import {onboardingURL} from './src/utils/constants'
 export default function App() {
+  const [_, setResult] = useState<WebBrowser.WebBrowserResult | null>(null)
+
+  const _handlePressButtonAsync = async () => {
+    let result = await WebBrowser.openBrowserAsync(onboardingURL)
+    setResult(result)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity style={styles.button} onPress={_handlePressButtonAsync}>
+        <Text style={styles.textButton}>Open Onboarding</Text>
+      </TouchableOpacity>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
+    padding: 10,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  button: {
+    borderRadius: 40,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#00b2fd',
+    padding: 10,
+    width: Dimensions.get('window').width - 50,
+  },
+  textButton: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+})
---
+++ src/utils/constants.ts
@@ -0,0 +1,2 @@
+export const onboardingURL =
+  'https://onboarding.incodesmile.com/incode/flow/6116690566b80b001531ca2a?region=ALL'
